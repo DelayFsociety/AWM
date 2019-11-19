8889ea7b2cc8d6762f1cab23e2bf292a376f81af
DelayFsociety <57909299+DelayFsociety@users.noreply.github.com>
2019-11-19 21:19:45 +0700

    Update awm-beta.py

--- awm-beta.py
+++ awm-beta.py
@@ -11,21 +11,21 @@ print "[00]>exit"
 print
 A = raw_input("AWM=>")
 
-if A == "1" or "01":
+if A == "1" or A == "01":
   print "github Script"
   print "https://github.com/Cabbagec/termux-ohmyzsh"
 
 
-elif A == "2" or "02":
+elif A == "2" or A == "02":
   os.system("clear")
   print "Link Download"
   print "Setup pip2 install mechanize"
   print "https://drive.google.com/file/d/1-4wJSvARyRw3IaR6RpWVbboKeg6HmyL6/view"
 
-elif A == "3" or "03":
+elif A == "3" or A == "03":
   os.system("clear")
   print "Link Download"
   print "https://www.mediafire.com/file/1c76plqgpoqr4pd/bash.bashrc.txt/file"
   
-elif A == "0" or "00":
+elif A == "0" or A == "00":
   sys.exit()
