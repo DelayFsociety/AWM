c87b7a8a20dca6ed3cb572b6ff0bf49e757f79a9
DelayFsociety <57909299+DelayFsociety@users.noreply.github.com>
2019-11-19 21:12:03 +0700

    Create awm-beta.py

---
+++ awm-beta.py
@@ -0,0 +1,31 @@
+import os, sys
+os.system("figlet AWM")
+print "Athor DeLay Fsociety"
+print "Group MALWARE"
+print "Version 1.1"
+print
+print "           [01]>Termux-ohmyzsh"
+print "           [02]>Root@localhost}"
+print "           [03]>Termux-Theme"
+print "[00]>exit"
+print
+A = raw_input("AWM=>")
+
+if A == "1" or "01":
+  print "github Script"
+  print "https://github.com/Cabbagec/termux-ohmyzsh"
+
+
+elif A == "2" or "02":
+  os.system("clear")
+  print "Link Download"
+  print "Setup pip2 install mechanize"
+  print "https://drive.google.com/file/d/1-4wJSvARyRw3IaR6RpWVbboKeg6HmyL6/view"
+
+elif A == "3" or "03":
+  os.system("clear")
+  print "Link Download"
+  print "https://www.mediafire.com/file/1c76plqgpoqr4pd/bash.bashrc.txt/file"
+  
+elif A == "0" or "00":
+  sys.exit()
